94efeab5ce68402e81ea25240deea9b03c743452
Albert Callarisa <shark234@gmail.com>
2014-09-03 08:44:16 +0800

    use `stdin` to get the URL

--- app.js
+++ app.js
@@ -3,42 +3,46 @@ var path = require('path'),
     PNG = require('pngjs').PNG,
     request = require('request');
 
-var url = process.argv[2];
+var url = "";
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', function() {
+  var chunk = process.stdin.read()
+  if (chunk){
+    url = url + chunk;
+  }
+});
 
-if(!url){
-  console.error("Usage: node resizer.js URL")
-  process.exit(1)
-}
-
-var reqStream = request.get({
-  "uri": url,
-  "encoding": null,
-  "strictSSL": false,
-  "timeout": 5000,
-  "pool.maxSockets": 10
-})
-
-var pixels = (" .,:;i1tfLCG08@").split("")
-
-gm(reqStream)
-.setFormat("png")
-.resize(170, 70, "!")
-.type("Grayscale")
-.stream(function (err, stdout) {
-  var stream = stdout.pipe(new PNG({ filterType: 4 }))
-  stream.on("parsed", function () {
-    var png = this;
-    var converted = "";
-    for (var y = 0; y < png.height; y++) {
-      for (var x = 0; x < png.width; x++) {
-        var idx = (png.width * y + x) << 2,
-            color = png.data[idx],
-            value = 14 - Math.floor(color / 18)
-
-        process.stdout.write(pixels[value])
-      }
-      process.stdout.write("\n")
-    }
-    process.exit(0)
+process.stdin.on('end', function() {
+  var reqStream = request.get({
+    "uri": url,
+    "encoding": null,
+    "strictSSL": false,
+    "timeout": 5000,
+    "pool.maxSockets": 10
   })
+
+  var pixels = (" .,:;i1tfLCG08@").split("")
+
+  gm(reqStream)
+  .setFormat("png")
+  .resize(170, 70, "!")
+  .type("Grayscale")
+  .stream(function (err, stdout) {
+    var stream = stdout.pipe(new PNG({ filterType: 4 }))
+    stream.on("parsed", function () {
+      var png = this;
+      var converted = "";
+      for (var y = 0; y < png.height; y++) {
+        for (var x = 0; x < png.width; x++) {
+          var idx = (png.width * y + x) << 2,
+              color = png.data[idx],
+              value = 14 - Math.floor(color / 18)
+
+          process.stdout.write(pixels[value])
+        }
+        process.stdout.write("\n")
+      }
+      process.exit(0)
+    })
+  });
 });
